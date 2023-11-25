ff5a6b35ba68204b3eaae6fecf31ac23c8e480e5
Mukhammad Bakhriddinov <salahbm.001@gmail.com>
2023-11-25 12:52:49 +0900

    header/content text

--- test/firstTest.js
+++ test/firstTest.js
@@ -5,7 +5,7 @@ const { assert } = require('chai');
 require('dotenv').config();
 const copyPaste = require('copy-paste');
 const path = require('path');
-
+const imgFile = path.resolve(__dirname, '../Tez-Yoz.png');
 const sleep = (milliseconds) => new Promise(resolve => setTimeout(resolve, milliseconds));
 
 async function loginToNaver(driver) {
@@ -69,141 +69,134 @@ async function readAndCopyFileToClipboard(filePath) {
   }
 }
 
-async function clickCancelButton(driver) {
-  try {
-    const cancelButton = await driver.findElement(By.css('.se-popup-button.se-popup-button-cancel'));
 
-    if (cancelButton) {
-      await cancelButton.click();
-    } else {
-      console.log('Cancel button is not displayed.');
-    }
-  } catch (error) {
-    console.error('Error handling pop-up:', error.message);
-  }
-}
 
 async function createBlog(driver) {
   try {
     // Clicking on "Do not register"
     try {
-      await driver.findElement(By.xpath('//*[@id="new.dontsave"]')).click();
+      await driver.findElement(By.xpath('//button[@id="new.dontsave"]')).click();
     } catch (error) {
       // NoSuchElementException
-      console.log('no element found', error.message);
+      console.log('No element found for "Do not register".');
     }
 
-    await driver.sleep(1000);
-
-
+    await sleep(1000);
     // Navigating to Naver Post
     const url = 'https://post.naver.com';
     await driver.get(url);
-    await driver.sleep(2000);
+    await sleep(2000);
 
     // Clicking on the "Write Post" button
     const postBtn = await driver.findElement(By.xpath('//*[@id="header"]/div[1]/a[3]'));
     await postBtn.click();
-    await driver.sleep(3000);
-
-    const txtFilePath = 'C:\\Users\\salah\\Documents\\reactjs\\selenium\\title.txt';
-    const lines = fs.readFileSync(txtFilePath, 'utf-8').split('\n');
-    const header = lines[0].split(',');
-
-    for (let i = 1; i < lines.length; i++) {
-      const values = lines[i].split(',');
-      const post = {};
-
-      for (let j = 0; j < header.length; j++) {
-        post[header[j]] = values[j];
-      }
-
-      const titleDirectory = post['title_2'];
-      const imgFileDirectory = post['img_directory'];
-      const txtFileDirectory = post['txt_directory'];
-
+    await driver.wait(
+      until.elementLocated(By.css('.in_layer')),
+      5000
+    );
+    
+    // Close the popup
+    const closeButton = await driver.wait(
+      until.elementLocated(By.css('.__se_pop_close')),
+      5000
+    );
+    await closeButton.click();
+    
+    // Wait until the popup is no longer visible
+    await driver.wait(
+      until.stalenessOf(closeButton),
+      5000
+    );
     
 
-      const contentList = fs.readFileSync(txtFileDirectory, 'utf-8').split('\n');
-      const bloggerTitle = titleDirectory;
-      const bloggerTitle2 = bloggerTitle.replace("\ufeff", "");
+    // Example header content
+    const headerContent = 'This is my first header by Selenium!!!';
 
-      const contentDesc = fs.readFileSync(txtFileDirectory, 'utf-8');
-      const tistoryTitle = String(bloggerTitle2);
+    // Naver Post Title Entry
+    const iFrames = await driver.findElements(By.tagName('iframe'));
+    await driver.switchTo().frame(iFrames[0]);
 
-      // Naver Post Title Entry
-      const iframes = await driver.findElements(By.tagName('iframe'));
-      await driver.switchTo().frame(iframes[0]);
+    const tagTitle = await driver.wait(
+      until.elementLocated(By.xpath("//textarea[@placeholder='제목']")),
+      5000
+    );
+    await tagTitle.click();
+    await sleep(1000);
+    await driver.actions().sendKeys(headerContent).perform();
+    await sleep(1000);
 
-      const tagTitle = await driver.findElement(By.xpath("//textarea[@placeholder='제목']"));
-      await tagTitle.click();
-      await driver.sleep(1000);
-      const title = String(tistoryTitle);
-      await driver.sleep(1000);
-      await driver.actions().sendKeys(title).perform();
-      await driver.actions().sendKeys(Key.NULL).perform();
-      await driver.sleep(1000);
 
+    const contentList = [
+      "Introduction: This is my blog post.",
+      "Body Paragraph 1: Here is some content for the first paragraph.",
+      "Body Paragraph 2: Adding more content to the second paragraph.",
+      "Conclusion: Wrapping up the blog post with a conclusion.",
+    ];
+    
+
+    
+    try {
       // Content Entry
       const tagContent = await driver.findElement(By.xpath("//div[@class='se_editView']"));
       await tagContent.click();
-      await driver.sleep(1000);
-
+      await sleep(1000);
+  
       // Introduction
       const content1 = String(contentList[0]);
       await driver.actions().sendKeys(content1).perform();
-      await driver.sleep(1000);
-
+      await sleep(1000);
+  
       // Body
-      for (let j = 1; j < contentList.length - 1; j++) {
+      for (let j = 0; j < contentList.length ; j++) {
         const content2 = String(contentList[j]);
-        await driver.actions().sendKeys(content2).perform();
-        await driver.sleep(1000);
+        await driver.actions().sendKeys(content2,'\n~~~~~~~~\n').perform();
+        await sleep(100);
       }
+  
 
-      // Adding image
-      if (imgFileDirectory !== 'no_image') {
-        // Adding image
-        const tagAddImg = await driver.findElement(By.xpath("//button[@class='se-2d62a39d se-ff2750f7 se-l-icon-plus']"));
-        await tagAddImg.click();
-        await driver.sleep(1000);
+      console.log(`file: firstTest.js:160 ~ imgFileDirectory:`, imgFile)
+      const tagAddImgButton = await driver.findElement(By.xpath('//ul[@id="se_side_comp_list"]//li[2]//button[contains(@class=se_component_image)]'));
+      await tagAddImgButton.click();
+      await sleep(1000);
+      
+      
 
-        // Switching to the image upload iframe
-        const iframes = await driver.findElements(By.tagName('iframe'));
-        await driver.switchTo().frame(iframes[2]);
-        await driver.sleep(1000);
-
-        // Uploading the image
-        const tagFileUpload = await driver.findElement(By.xpath("//input[@type='file']"));
-        await tagFileUpload.sendKeys(imgFileDirectory);
-        await driver.sleep(1000);
-
-        // Waiting for the image to upload
-        await driver.sleep(5000);
-
-        // Switching back to the main iframe
-        await driver.switchTo().defaultContent();
-        await driver.sleep(1000);
-      }
-
+      
+      // Switching to the image upload iframe
+      const iframes = await driver.findElements(By.tagName('iframe'));
+      await driver.switchTo().frame(iframes[2]);
+      await sleep(1000);
+      
+      // Find the file input element
+      const tagFileUpload = await driver.findElement(By.xpath("//input[@type='file']"));
+      
+      // Directly set the value of the file input element
+      await tagFileUpload.sendKeys(imgFile);
+      
+      // Wait for the image to upload
+      await sleep(5000);
+      
+      // Switching back to the main iframe
+      await driver.switchTo().defaultContent();
+      await sleep(1000);
+  
       // Posting
       const tagPostBtn = await driver.findElement(By.xpath("//button[@class='se-3e1641a2 se-ff2750f7 se-ff2750f7-primary-large']"));
       await tagPostBtn.click();
-      await driver.sleep(5000);
-
-      // Writing to the Excel file (if needed)
-      // healthNaverPosting[i]['naver_posting'] = 'O';
-
+      await sleep(5000);
+  
+      // Write to the Excel file or perform other actions as needed
+  
       // Waiting before moving on to the next post
-      await driver.sleep(2000);
+      await sleep(2000);
+    } catch (error) {
+      console.log('Error creating blog with image:', error.message);
     }
-
-    // Switch back to the default content
-    await driver.switchTo().defaultContent();
   } catch (error) {
     console.log('Error creating blog:', error.message);
   }
 }
+
 
 
 describe("Automated Blogging on Naver", function () {
@@ -216,18 +209,15 @@ describe("Automated Blogging on Naver", function () {
       await loginToNaver(driver);
       assert.isTrue(await driver.wait(until.urlIs('https://www.naver.com/'), 5000), 'Login successful');
 
-      const img = `Tez-Yoz.png`
-      const txt = 'C:\\Users\\salah\\Documents\\reactjs\\selenium\\title.txt';
+      // const img = `Tez-Yoz.png`
+      // const txt = 'C:\\Users\\salah\\Documents\\reactjs\\selenium\\title.txt';
 
-      await createBlog(driver, img, txt);
-
-      console.log('Test completed successfully');
+      await createBlog(driver);
     } catch (error) {
       assert.fail('Test failed: ' + error.message);
     } finally {
       if (driver) {
-        await driver.quit();
-        console.log('WebDriver quit');
+        // await driver.quit();
       }
     }
   });
--- test/pythonTest.py
+++ test/pythonTest.py
@@ -1,280 +1,280 @@
-# # Importing necessary libraries
-# from oauth2client.service_account import ServiceAccountCredentials
-# import os
-# from googleapiclient.discovery import build
-# from httplib2 import Http
-# from oauth2client import file, client, tools
-# from googleapiclient.http import MediaFileUpload
-# from __future__ import print_function
-# import pickle
-# import os.path
-# from googleapiclient.discovery import build
-# from google_auth_oauthlib.flow import InstalledAppFlow
-# from google.auth.transport.requests import Request
-# import warnings
-# from selenium import webdriver
-# from selenium.webdriver.support import expected_conditions as EC
-# from selenium.webdriver.common.keys import Keys 
-# from selenium.webdriver.common.action_chains import ActionChains
-# from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
-# from bs4 import BeautifulSoup
-# import time
-# import pyperclip
-# import requests
-# import datetime
-# import pymssql
-# import pandas as pd
-# from pandas.core.frame import DataFrame
-# import matplotlib.pyplot as plt
-# import chromedriver_autoinstaller
-# import subprocess
-# from selenium.webdriver.chrome.options import Options
-# from selenium.webdriver.common.alert import Alert
-# import chromedriver_autoinstaller
-# import subprocess
-# import shutil 
-# import xlrd
-# import openpyxl 
-# import pygsheets
-# import csv
-# import re
-# import webbrowser
-# import os
-# import sys
-# import urllib.request
-# import json
-# from pandas.io.json import json_normalize
-# import hashlib
-# import hmac
-# import base64
-# import numpy as np
-# import autoit  # autoit must be installed
+# Importing necessary libraries
+from oauth2client.service_account import ServiceAccountCredentials
+import os
+from googleapiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file, client, tools
+from googleapiclient.http import MediaFileUpload
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import warnings
+from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys 
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
+from bs4 import BeautifulSoup
+import time
+import pyperclip
+import requests
+import datetime
+import pymssql
+import pandas as pd
+from pandas.core.frame import DataFrame
+import matplotlib.pyplot as plt
+import chromedriver_autoinstaller
+import subprocess
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.alert import Alert
+import chromedriver_autoinstaller
+import subprocess
+import shutil 
+import xlrd
+import openpyxl 
+import pygsheets
+import csv
+import re
+import webbrowser
+import os
+import sys
+import urllib.request
+import json
+from pandas.io.json import json_normalize
+import hashlib
+import hmac
+import base64
+import numpy as np
+import autoit  # autoit must be installed
 
-# # Suppressing warnings
-# warnings.filterwarnings('ignore')
+# Suppressing warnings
+warnings.filterwarnings('ignore')
 
-# # Uncomment the code below if you want to delete cookies and cache files
-# # try:
-# #     shutil.rmtree(r"c:\chrometemp")  # Delete cookies and cache files
-# # except FileNotFoundError:
-# #     pass
-
-# # Launching Chrome with remote debugging port
-# subprocess.Popen(r'C:\Program Files\Google\Chrome\Application\chrome.exe --remote-debugging-port=9222 --user-data-dir="C:\chrometemp"')
-
-# # Setting up Chrome options
-# option = Options()
-# option.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
-# chrome_ver = chromedriver_autoinstaller.get_chrome_version().split('.')[0]
+# Uncomment the code below if you want to delete cookies and cache files
 # try:
-#     driver = webdriver.Chrome(f'./{chrome_ver}/chromedriver.exe', options=option)
-# except:
-#     chromedriver_autoinstaller.install(True)
-#     driver = webdriver.Chrome(f'./{chrome_ver}/chromedriver.exe', options=option)
-
-# # Maximizing window and setting up implicit wait
-# driver.maximize_window()  # Maximize window
-# time.sleep(3)
-# driver.implicitly_wait(10)
-
-# # Setting up ActionChains
-# action = ActionChains(driver)
-
-# # Navigating to Naver
-# url = 'https://www.naver.com'
-# driver.get(url)
-# time.sleep(5)
-# driver.implicitly_wait(10)
-
-# # Clicking on the login link
-# driver.find_element_by_class_name('link_login').click()
-# time.sleep(1)
-# driver.implicitly_wait(10)
-
-# # Finding elements for ID and password
-# tag_id = driver.find_element_by_name('id')
-# tag_pw = driver.find_element_by_name('pw')
-# driver.implicitly_wait(10)
-
-# # Entering ID
-# tag_id.click()
-# pyperclip.copy('id') 
-# tag_id.send_keys(Keys.CONTROL, 'v')
-# time.sleep(2)
-# driver.implicitly_wait(10)
-
-# # Entering password
-# tag_pw.click()
-# pyperclip.copy('pw')
-# tag_pw.send_keys(Keys.CONTROL, 'v')
-# time.sleep(2)
-# driver.implicitly_wait(10)
-
-# # Clicking on the login button
-# login_btn = driver.find_element_by_id('log.login')
-# login_btn.click()
-# time.sleep(1)
-# driver.implicitly_wait(10)
-
-# # Handling registration prompt
-# try:
-#     driver.find_element_by_xpath('//*[@id="new.dontsave"]').click()  # Clicking on "Do not register"
-# except NoSuchElementException:
+#     shutil.rmtree(r"c:\chrometemp")  # Delete cookies and cache files
+# except FileNotFoundError:
 #     pass
-# time.sleep(1)
-# driver.implicitly_wait(10)
 
-# # Navigating to Naver Post
-# url = 'https://post.naver.com'
-# driver.get(url)
-# time.sleep(2)
-# driver.implicitly_wait(10)
+# Launching Chrome with remote debugging port
+subprocess.Popen(r'C:\Program Files\Google\Chrome\Application\chrome.exe --remote-debugging-port=9222 --user-data-dir="C:\chrometemp"')
 
-# # Clicking on the "Write Post" button
-# post_btn = driver.find_element_by_xpath('//*[@id="header"]/div[1]/a[3]')
-# post_btn.click()
-# time.sleep(3)
-# driver.implicitly_wait(10)
+# Setting up Chrome options
+option = Options()
+option.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
+chrome_ver = chromedriver_autoinstaller.get_chrome_version().split('.')[0]
+try:
+    driver = webdriver.Chrome(f'./{chrome_ver}/chromedriver.exe', options=option)
+except:
+    chromedriver_autoinstaller.install(True)
+    driver = webdriver.Chrome(f'./{chrome_ver}/chromedriver.exe', options=option)
 
-# # Handling pop-up
-# # position = pyautogui.position()  # Get cursor position
-# # print(pyautogui.size())  # Check screen size
-# # while True:
-# #     print("Current Mouse Position: ", pyautogui.position())
-# #     time.sleep(1)
+# Maximizing window and setting up implicit wait
+driver.maximize_window()  # Maximize window
+time.sleep(3)
+driver.implicitly_wait(10)
 
-# # Automatically posting
-# health_naver_posting = pd.read_excel('C:/Users/user/Desktop/folder/folder/blog_viewracle.xlsx')
+# Setting up ActionChains
+action = ActionChains(driver)
 
-# for i in range(1, 16):
-#     url = 'https://post.editor.naver.com/editor'
-#     driver.get(url)
+# Navigating to Naver
+url = 'https://www.naver.com'
+driver.get(url)
+time.sleep(5)
+driver.implicitly_wait(10)
+
+# Clicking on the login link
+driver.find_element_by_class_name('link_login').click()
+time.sleep(1)
+driver.implicitly_wait(10)
+
+# Finding elements for ID and password
+tag_id = driver.find_element_by_name('id')
+tag_pw = driver.find_element_by_name('pw')
+driver.implicitly_wait(10)
+
+# Entering ID
+tag_id.click()
+pyperclip.copy('id') 
+tag_id.send_keys(Keys.CONTROL, 'v')
+time.sleep(2)
+driver.implicitly_wait(10)
+
+# Entering password
+tag_pw.click()
+pyperclip.copy('pw')
+tag_pw.send_keys(Keys.CONTROL, 'v')
+time.sleep(2)
+driver.implicitly_wait(10)
+
+# Clicking on the login button
+login_btn = driver.find_element_by_id('log.login')
+login_btn.click()
+time.sleep(1)
+driver.implicitly_wait(10)
+
+# Handling registration prompt
+try:
+    driver.find_element_by_xpath('//*[@id="new.dontsave"]').click()  # Clicking on "Do not register"
+except NoSuchElementException:
+    pass
+time.sleep(1)
+driver.implicitly_wait(10)
+
+# Navigating to Naver Post
+url = 'https://post.naver.com'
+driver.get(url)
+time.sleep(2)
+driver.implicitly_wait(10)
+
+# Clicking on the "Write Post" button
+post_btn = driver.find_element_by_xpath('//*[@id="header"]/div[1]/a[3]')
+post_btn.click()
+time.sleep(3)
+driver.implicitly_wait(10)
+
+# Handling pop-up
+# position = pyautogui.position()  # Get cursor position
+# print(pyautogui.size())  # Check screen size
+# while True:
+#     print("Current Mouse Position: ", pyautogui.position())
 #     time.sleep(1)
-#     driver.implicitly_wait(10)
-#     action = ActionChains(driver)
 
-#     # Handling leaving site and pressing Enter
-#     try:
-#         alert = driver.switch_to.alert
-#         message = alert.text
-#         print("Alert shows following message: " + message)
-#         time.sleep(1)
-#         alert.accept()
-#         time.sleep(1)
-#         driver.implicitly_wait(10)
-#         driver.get(url)
-#         time.sleep(1)
-#         driver.send_keys(Keys.ENTER)
-#         driver.implicitly_wait(10)
-#     except:
-#         print("no alert")
+# Automatically posting
+health_naver_posting = pd.read_excel('C:/Users/user/Desktop/folder/folder/blog_viewracle.xlsx')
 
-#     # Handling another alert and dismissing it
-#     try:
-#         alert = driver.switch_to.alert
-#         message = alert.text
-#         print("Alert shows following message: " + message)
-#         time.sleep(2)
-#         alert.dismiss()
-#         time.sleep(1)
-#         driver.implicitly_wait(10)
-#     except:
-#         print("no alert")
+for i in range(1, 16):
+    url = 'https://post.editor.naver.com/editor'
+    driver.get(url)
+    time.sleep(1)
+    driver.implicitly_wait(10)
+    action = ActionChains(driver)
 
-#     time.sleep(1) 
-#     driver.implicitly_wait(10)
+    # Handling leaving site and pressing Enter
+    try:
+        alert = driver.switch_to.alert
+        message = alert.text
+        print("Alert shows following message: " + message)
+        time.sleep(1)
+        alert.accept()
+        time.sleep(1)
+        driver.implicitly_wait(10)
+        driver.get(url)
+        time.sleep(1)
+        driver.send_keys(Keys.ENTER)
+        driver.implicitly_wait(10)
+    except:
+        print("no alert")
 
-#     title_directory = health_naver_posting.loc[i]['title_2']
-#     img_file_directory = health_naver_posting.loc[i]['img_directory']
-#     txt_file_directory = health_naver_posting.loc[i]['txt_directory']
-#     img_dir = img_file_directory
+    # Handling another alert and dismissing it
+    try:
+        alert = driver.switch_to.alert
+        message = alert.text
+        print("Alert shows following message: " + message)
+        time.sleep(2)
+        alert.dismiss()
+        time.sleep(1)
+        driver.implicitly_wait(10)
+    except:
+        print("no alert")
 
-#     f = open(txt_file_directory, 'r', encoding='utf-8')
-#     f_2 = open(txt_file_directory, 'r', encoding='utf-8')
+    time.sleep(1) 
+    driver.implicitly_wait(10)
 
-#     content_list = f.readlines()
-#     blogger_title = title_directory
-#     blogger_title_2 = blogger_title.replace("\ufeff", "") 
+    title_directory = health_naver_posting.loc[i]['title_2']
+    img_file_directory = health_naver_posting.loc[i]['img_directory']
+    txt_file_directory = health_naver_posting.loc[i]['txt_directory']
+    img_dir = img_file_directory
 
-#     content_desc = f_2.read()
-#     tistory_title = str(blogger_title_2)
+    f = open(txt_file_directory, 'r', encoding='utf-8')
+    f_2 = open(txt_file_directory, 'r', encoding='utf-8')
 
-#     # Naver Post Title Entry
-#     iframes = driver.find_elements_by_tag_name('iframe')
-#     driver.switch_to.frame(iframes[0])
+    content_list = f.readlines()
+    blogger_title = title_directory
+    blogger_title_2 = blogger_title.replace("\ufeff", "") 
 
-#     tag_title = driver.find_element_by_xpath("//textarea[@placeholder='제목']")
-#     tag_title.click()
-#     time.sleep(1) 
-#     title = str(tistory_title)
-#     time.sleep(1) 
-#     action.send_keys(title).perform()
-#     action = ActionChains(driver)
-#     time.sleep(1)
-#     driver.implicitly_wait(10)
+    content_desc = f_2.read()
+    tistory_title = str(blogger_title_2)
 
-#     # Content Entry
-#     tag_content = driver.find_element_by_xpath("//div[@class='se_editView']")
-#     tag_content.click()
-#     time.sleep(1)
-#     driver.implicitly_wait(10)
+    # Naver Post Title Entry
+    iframes = driver.find_elements_by_tag_name('iframe')
+    driver.switch_to.frame(iframes[0])
 
-#     # Introduction
-#     content_1 = str(content_list[0])
-#     action.send_keys(content_1).perform()
-#     action = ActionChains(driver)
-#     time.sleep(1)
-#     driver.implicitly_wait(10)
+    tag_title = driver.find_element_by_xpath("//textarea[@placeholder='제목']")
+    tag_title.click()
+    time.sleep(1) 
+    title = str(tistory_title)
+    time.sleep(1) 
+    action.send_keys(title).perform()
+    action = ActionChains(driver)
+    time.sleep(1)
+    driver.implicitly_wait(10)
 
-#     # Body
-#     for j in range(1, len(content_list) - 1):
-#         content_2 = str(content_list[j])
-#         action.send_keys(content_2).perform()
-#         action = ActionChains(driver)
-#         time.sleep(1)
-#         driver.implicitly_wait(10)
+    # Content Entry
+    tag_content = driver.find_element_by_xpath("//div[@class='se_editView']")
+    tag_content.click()
+    time.sleep(1)
+    driver.implicitly_wait(10)
 
-#     # Adding image
-#     if img_file_directory != 'no_image':
-#         # Adding image
-#         tag_add_img = driver.find_element_by_xpath("//button[@class='se-2d62a39d se-ff2750f7 se-l-icon-plus']") 
-#         tag_add_img.click()
-#         time.sleep(1)
-#         driver.implicitly_wait(10)
+    # Introduction
+    content_1 = str(content_list[0])
+    action.send_keys(content_1).perform()
+    action = ActionChains(driver)
+    time.sleep(1)
+    driver.implicitly_wait(10)
 
-#         # Switching to the image upload iframe
-#         iframes = driver.find_elements_by_tag_name('iframe')
-#         driver.switch_to.frame(iframes[2])
-#         time.sleep(1)
-#         driver.implicitly_wait(10)
+    # Body
+    for j in range(1, len(content_list) - 1):
+        content_2 = str(content_list[j])
+        action.send_keys(content_2).perform()
+        action = ActionChains(driver)
+        time.sleep(1)
+        driver.implicitly_wait(10)
 
-#         # Uploading the image
-#         tag_file_upload = driver.find_element_by_xpath("//input[@type='file']")
-#         tag_file_upload.send_keys(img_file_directory)
-#         time.sleep(1)
-#         driver.implicitly_wait(10)
+    # Adding image
+    if img_file_directory != 'no_image':
+        # Adding image
+        tag_add_img = driver.find_element_by_xpath("//button[@class='se-2d62a39d se-ff2750f7 se-l-icon-plus']") 
+        tag_add_img.click()
+        time.sleep(1)
+        driver.implicitly_wait(10)
 
-#         # Waiting for the image to upload
-#         time.sleep(5)
-#         driver.implicitly_wait(10)
+        # Switching to the image upload iframe
+        iframes = driver.find_elements_by_tag_name('iframe')
+        driver.switch_to.frame(iframes[2])
+        time.sleep(1)
+        driver.implicitly_wait(10)
 
-#         # Switching back to the main iframe
-#         driver.switch_to.default_content()
-#         time.sleep(1)
-#         driver.implicitly_wait(10)
+        # Uploading the image
+        tag_file_upload = driver.find_element_by_xpath("//input[@type='file']")
+        tag_file_upload.send_keys(img_file_directory)
+        time.sleep(1)
+        driver.implicitly_wait(10)
 
-#     # Posting
-#     tag_post_btn = driver.find_element_by_xpath("//button[@class='se-3e1641a2 se-ff2750f7 se-ff2750f7-primary-large']") 
-#     tag_post_btn.click()
-#     time.sleep(5)
-#     driver.implicitly_wait(10)
+        # Waiting for the image to upload
+        time.sleep(5)
+        driver.implicitly_wait(10)
 
-#     # Writing to the Excel file
-#     health_naver_posting.loc[i, 'naver_posting'] = 'O'
+        # Switching back to the main iframe
+        driver.switch_to.default_content()
+        time.sleep(1)
+        driver.implicitly_wait(10)
 
-#     # Waiting before moving on to the next post
-#     time.sleep(2)
-#     driver.implicitly_wait(10)
+    # Posting
+    tag_post_btn = driver.find_element_by_xpath("//button[@class='se-3e1641a2 se-ff2750f7 se-ff2750f7-primary-large']") 
+    tag_post_btn.click()
+    time.sleep(5)
+    driver.implicitly_wait(10)
 
-# # Exiting the browser
-# driver.quit()
+    # Writing to the Excel file
+    health_naver_posting.loc[i, 'naver_posting'] = 'O'
+
+    # Waiting before moving on to the next post
+    time.sleep(2)
+    driver.implicitly_wait(10)
+
+# Exiting the browser
+driver.quit()
